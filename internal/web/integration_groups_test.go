af74bb6a833d8eeb8d1c6ee03b1c2da414814805
lostmaniac <maniac.cn@gmail.com>
2026-05-15 00:23:09 +0800

    fix: 移除未使用的 config 导入

--- internal/web/integration_groups_test.go
+++ internal/web/integration_groups_test.go
@@ -8,7 +8,6 @@ import (
 
   "github.com/picoaide/picoaide/internal/auth"
   "github.com/picoaide/picoaide/internal/authsource"
-  "github.com/picoaide/picoaide/internal/config"
 )
 
 func TestWhitelist_GetEmpty(t *testing.T) {
